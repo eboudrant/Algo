21fa259252334d2271c3aa996dbfed2ecb7b40cd
Emmanuel Boudrant <emmanuel.boudrant@gmail.com>
2011-05-08 12:00:25 +0900

    remove from default package

--- Biggest64bitUnsignedInteger/src/main/java/algo/BiggestIntegersFromFiles.java
+++ Biggest64bitUnsignedInteger/src/main/java/algo/BiggestIntegersFromFiles.java
@@ -1,3 +1,4 @@
+package algo;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
--- Biggest64bitUnsignedInteger/src/test/java/algo/BiggestIntegersFromFilesTest.java
+++ Biggest64bitUnsignedInteger/src/test/java/algo/BiggestIntegersFromFilesTest.java
@@ -1,3 +1,4 @@
+package algo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
